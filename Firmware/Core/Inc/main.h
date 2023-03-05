ca43266b526925476f410af32d130f7eb0bf3679
EmilsSturitis <emils.sturitis@gmail.com>
2023-03-05 11:10:45 +0100

    added Control_Blade from universal project

--- Firmware/Core/Inc/main.h
+++ Firmware/Core/Inc/main.h
@@ -57,6 +57,14 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define LED_3_Pin GPIO_PIN_2
+#define LED_3_GPIO_Port GPIOC
+#define LED_4_Pin GPIO_PIN_3
+#define LED_4_GPIO_Port GPIOC
+#define LED_1_Pin GPIO_PIN_12
+#define LED_1_GPIO_Port GPIOB
+#define LED_2_Pin GPIO_PIN_13
+#define LED_2_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 
